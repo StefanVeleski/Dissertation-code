32586a3b5a76a8072fe5e519f03aa6b706ea6fd1
Stefan Veleski <stefan_veleski@hotmail.com>
2020-08-06 21:42:40 +0200

    Update Dissertation code.R
    1

--- Dissertation code.R
+++ Dissertation code.R
@@ -14,5 +14,18 @@ ggbetweenstats(data = gender_df,
     scale_y_log10()
 
 
+####Correlation of Goodreads and Open Syllabus data####
+library(ggpubr)
+options(scipen=10000)
+names(Main_dataset)
+str(Main_dataset)
 
-
+corr_plot <- ggscatter(Main_dataset, x = "Ratings", y = "Syllabi",
+                       color = 'darkturquoise',
+                       add = "reg.line", conf.int = TRUE,
+                       xscale = "log10",
+                       yscale = "log10",
+                       cor.coef = TRUE, cor.method = "pearson",
+                       title = "Correlation plot (full dataset)",
+                       xlab = "Number of Goodreads ratings (log)", ylab = "Open Syllabus")
+corr_plot
