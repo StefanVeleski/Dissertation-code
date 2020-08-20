d705ef3f19f9b6e76e303b3444334e8b2257dbc9
Stefan Veleski <stefan_veleski@hotmail.com>
2020-08-20 19:02:40 +0200

    1
    1

--- Dissertation code.R
+++ Dissertation code.R
@@ -1,31 +1,32 @@
 ####Gender and contemporary success####
 library(tidyverse)
-gender_df <- select(Main_dataset, Year, Name, Gender, Title, `Gutenberg ID`, `Number of Ratings`, `Bestseller?`)
+options(scipen=10000)
+Main_dataset$Gender <- as.factor(Main_dataset$Gender)
 
 set.seed(123)
 
-options(scipen=10000)
 library("ggstatsplot")
-ggbetweenstats(data = gender_df, 
-               x = Gender, 
-               y = `Number of Ratings`,
-               xlab = "Author's gender",
+gender_plot <- ggbetweenstats(data = Main_dataset, 
+               x = Gender,
+               y = Ratings,
+               xlab = "The Author's Gender",
                ylab = "Number of Ratings (log)") +
     scale_y_log10()
-
+gender_plot
 
 ####Correlation of Goodreads and Open Syllabus data####
 library(ggpubr)
+
 options(scipen=10000)
 names(Main_dataset)
 str(Main_dataset)
 
 corr_plot <- ggscatter(Main_dataset, x = "Ratings", y = "Syllabi",
-                       color = 'darkturquoise',
+                       color = "#00AFBB",
                        add = "reg.line", conf.int = TRUE,
                        xscale = "log10",
                        yscale = "log10",
                        cor.coef = TRUE, cor.method = "pearson",
                        title = "Correlation plot (full dataset)",
                        xlab = "Number of Goodreads ratings (log)", ylab = "Open Syllabus")
-corr_plot
+corr_plot 
