7a96965bb531035e3ad82d7020b1355438be40e4
Stefan Veleski <stefan_veleski@hotmail.com>
2022-08-02 14:50:35 +0200

    Removed MATTR and Flesch from composit plot to avoid overlaps, new Figure 4.3, changes to author names in main dataset to avoid duplicates due to double spacing

--- Figure 4.3.R
+++ Figure 4.3.R
@@ -3,10 +3,11 @@
 library(readr)
 library(tidyverse)
 library(ggpubr)
+# The parts of this sript that are commented out are leftovers from a previous draft of the code, where the resulting figure was supposed to go in Subchapter 4.2, instead of Subchapter 4.1. As some of this information is repeated in Tables 4.1 and 4.2, I decided to just keep the comparison boxplot of sentiment. After all, this information, related to the secondary hypothesis of Subchapter 4.1, was lacking in the original article. 
 
-sentiment_flesch <- read_csv("Datasets and auxiliary files/sentiment_flesch.csv")
+# sentiment_flesch <- read_csv("Datasets and auxiliary files/sentiment_flesch.csv")
 Sentiment_dataset1 <- read_csv("Datasets and auxiliary files/Sentiment_dataset1.csv")
-sentiment_MATTR_dataset <- read_csv("Datasets and auxiliary files/sentiment_MATTR.csv")
+# sentiment_MATTR_dataset <- read_csv("Datasets and auxiliary files/sentiment_MATTR.csv")
 
 #### Comparison box plot of sentiment ####
 
@@ -17,12 +18,12 @@ sentiment_MATTR_dataset <- read_csv("Datasets and auxiliary files/sentiment_MATT
 means_sentiment <- aggregate(Sentiment ~  Status, Sentiment_dataset1, mean)
 means_sentiment$Sentiment <- format(round(means_sentiment$Sentiment, 4), nsmall = 4)
 
-A <- ggplot(Sentiment_dataset1, aes(x=Sentiment, y=Status)) + 
+ggplot(Sentiment_dataset1, aes(x=Sentiment, y=Status)) + 
   geom_boxplot(outlier.shape = NA, aes(color=Status))+ 
   geom_jitter(alpha=0.4, aes(color=Status)) + 
   stat_summary(fun=mean, geom="point", shape=15, size=4, aes(color=Status))+
   stat_summary(fun=mean, geom="text", size = 5, vjust=-1, aes(label=round(..x.., digits=4)))+
-  labs(title="Comparison of sentiment across facets",x="Sentiment", y = "Status") +
+  labs(x="Sentiment", y = "Status") +
   theme_minimal() + 
   theme(legend.position="none") + 
   coord_flip()
@@ -34,19 +35,19 @@ A <- ggplot(Sentiment_dataset1, aes(x=Sentiment, y=Status)) +
 # sentiment_MATTR_dataset_new <- droplevels(sentiment_MATTR_dataset[!sentiment_MATTR_dataset$Status == 'Neither',])
 
 # Calculating the means only, used in the text
-means_MATTR <- aggregate(MATTR ~  Status, sentiment_MATTR_dataset, mean)
-means_MATTR$MATTR <- format(round(means_MATTR$MATTR, 4), nsmall = 4)
+#means_MATTR <- aggregate(MATTR ~  Status, sentiment_MATTR_dataset, mean)
+#means_MATTR$MATTR <- format(round(means_MATTR$MATTR, 4), nsmall = 4)
 
 
-B <- ggplot(sentiment_MATTR_dataset, aes(x=MATTR, y=Status)) + 
-  geom_boxplot(outlier.shape = NA, aes(color=Status))+ 
-  geom_jitter(alpha=0.5, aes(color=Status)) + 
-  stat_summary(fun=mean, geom="point", shape=15, size=4, aes(color=Status))+
-  stat_summary(fun=mean, geom="text", size = 5, vjust=-1, aes(label=round(..x.., digits=4)))+
-  labs(title="Comparison of MATTR across facets",x="MATTR", y = "Status") +
-  theme_minimal() +
-  theme(legend.position="none") +
-  coord_flip()
+#B <- ggplot(sentiment_MATTR_dataset, aes(x=MATTR, y=Status)) + 
+#  geom_boxplot(outlier.shape = NA, aes(color=Status))+ 
+#  geom_jitter(alpha=0.5, aes(color=Status)) + 
+#  stat_summary(fun=mean, geom="point", shape=15, size=4, aes(color=Status))+
+#  stat_summary(fun=mean, geom="text", size = 5, vjust=-1, aes(label=round(..x.., digits=4)))+
+#  labs(title="Comparison of MATTR across facets",x="MATTR", y = "Status") +
+#  theme_minimal() +
+#  theme(legend.position="none") +
+#  coord_flip()
 
 #### Comparison box plot of Flesch scores ####
 
@@ -54,25 +55,25 @@ B <- ggplot(sentiment_MATTR_dataset, aes(x=MATTR, y=Status)) +
 # sentiment_flesch_new <- droplevels(sentiment_flesch[!sentiment_flesch$Status == 'Neither',])
 
 # Calculating the means only, used in the text
-means_Flesch <- aggregate(Flesch ~  Status, sentiment_flesch, mean)
-means_Flesch$Flesch <- format(round(means_Flesch$Flesch, 4), nsmall = 4)
+#means_Flesch <- aggregate(Flesch ~  Status, sentiment_flesch, mean)
+#means_Flesch$Flesch <- format(round(means_Flesch$Flesch, 4), nsmall = 4)
 
-C <- ggplot(sentiment_flesch, aes(x=Flesch, y=Status)) + 
-  geom_boxplot(outlier.shape = NA, aes(color=Status))+ 
-  geom_jitter(alpha=0.5, aes(color=Status)) + 
-  stat_summary(fun=mean, geom="point", shape=15, size=4, aes(color=Status))+
-  stat_summary(fun=mean, geom="text", size = 5, vjust=-1, aes(label=round(..x.., digits=4)))+
-  labs(title="Comparison of Flesch scores across facets",x="Flesch", y = "Status") +
-  theme_minimal() + 
-  theme(legend.position="none") +
-  coord_flip()
+#C <- ggplot(sentiment_flesch, aes(x=Flesch, y=Status)) + 
+#  geom_boxplot(outlier.shape = NA, aes(color=Status))+ 
+#  geom_jitter(alpha=0.5, aes(color=Status)) + 
+#  stat_summary(fun=mean, geom="point", shape=15, size=4, aes(color=Status))+
+#  stat_summary(fun=mean, geom="text", size = 5, vjust=-1, aes(label=round(..x.., digits=4)))+
+#  labs(title="Comparison of Flesch scores across facets",x="Flesch", y = "Status") +
+#  theme_minimal() + 
+#  theme(legend.position="none") +
+#  coord_flip()
 
 #### Merging the three plots together ####
-ggarrange(A, B, C,
-          labels = c("1", "2", "3"),
-          ncol = 1, nrow = 3
-)
+#ggarrange(A, B, C,
+#          labels = c("1", "2", "3"),
+#          ncol = 1, nrow = 3
+#)
 
 #### Exporting the plot ####
 
-ggsave(file = "Raw plots/Figure 4.3 (raw).pdf", dpi = 300, width = 2250, height = 3131, units = "px")
+ggsave(file = "Raw plots/Figure 4.3 (raw).pdf", dpi = 300)
