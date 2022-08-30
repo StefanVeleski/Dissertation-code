196793c59542ec1a780a023f8b98f571908a5316
Stefan Veleski <stefan_veleski@hotmail.com>
2022-08-30 12:16:47 +0200

    Final update
    Removed plot comparing the cultural longevity of Dracula and The Beetle

--- Figure 6.1.r
+++ Figure 6.1.r
@@ -1,75 +1,40 @@
-#### Differences in popularity and prestige between Dracula and The Beetle ####
+#### Film adaptations of Dracula and the Beetle####
 
-# Loading in the data (for more information about the data collection process, see the CHR2020 repository)
+# Loading in the data (for more information about the data collection process, see the CHR2020 repository on my Github profile)
 
-library(readr)
-Main_dataset <- read_csv("Datasets and auxiliary files/Main dataset.csv")
-
-# Creating a filtered dataset containing the Beetle and Dracula
-
-highlight_df <- Main_dataset %>%
-  filter(Rank1 == 30 | Rank1 == 2)
-
-# Creating the plots
-
-library(ggpubr)
+library(ggExtra)
 library(ggrepel)
-library(ggforce)
+library(readr)
+Dracula_adaptations <- read_csv("Datasets and auxiliary files/Dracula adaptations.csv")
 
-options(scipen = 10000)
+# Labeling several noteworthy adaptations
 
-E <- ggplot(Main_dataset, aes(x = Rank1, y = Ratings)) +
-  geom_point(alpha = 0.8, size = 3, color = "#F8766D") +
-  geom_point(
-    data = highlight_df,
-    aes(x = Rank1, y = Ratings),
-    color = "firebrick4",
-    size = 4
-  ) +
-  labs(
-    title = "Distribution of present day popularity",
-    x = "Rank",
-    y = "Number of Goodreads ratings"
-  ) +
-  facet_zoom(xlim = c(1, 35))
+ix_label <- c(66,2,3,68,60,69)
+Dracula_adaptations$Title[-ix_label] <- ""
+Dracula_adaptations$Title[ix_label] <- rownames()[ix_label]
 
-E <- E + geom_label_repel(
-  data = highlight_df, (aes(label = highlight_df$Title)),
-  box.padding = 0.35,
-  point.padding = 0.5,
-  segment.color = "grey50"
-)
+options(ggrepel.max.overlaps = Inf) 
 
-E
+# Creating the plot
 
-F <- ggplot(Main_dataset, aes(x = Rank2, y = Syllabi)) +
-  geom_point(alpha = 0.8, size = 3, color = "#00BFC4") +
-  geom_point(
-    data = highlight_df,
-    aes(x = Rank2, y = Syllabi),
-    color = "deepskyblue4",
-    size = 4
-  ) +
-  labs(
-    title = "Distribution of academic prestige",
-    x = "Rank",
-    y = "Number of Open Syllabus entries"
-  ) +
-  facet_zoom(xlim = c(1, 35))
+plot2 <- ggplot(Dracula_adaptations, aes(Year, ImdB, shape = `Book?`, label = Title)) +
+    geom_point(alpha = 0.7, size = 3, color = ifelse(Dracula_adaptations$Title == "", "gray48", "gray20"))+
+    geom_label_repel(aes(label = Dracula_adaptations$Title),
+    box.padding = 0.35, 
+    point.padding = 0.5,
+    segment.color = 'grey50') +
+    theme(legend.title = element_blank(), 
+          legend.position="bottom", 
+          legend.direction = "horizontal") +
+   labs(y = "Number of IMDb ratings")
+plot2
 
-F <- F + geom_label_repel(
-  data = highlight_df, (aes(label = highlight_df$Title)),
-  box.padding = 0.35,
-  point.padding = 0.5,
-  segment.color = "grey50"
-)
+# Adding marginal histograms
 
-F
+plot2 <- ggMarginal(plot2, type="histogram")
 
-composite_plot <- ggarrange(E, F, ncol = 2, nrow = 1)
-
-composite_plot
+plot2
 
 # Exporting the plot
 
-ggsave(file = "Raw plots/Figure 6.1 (raw).pdf", dpi = 300)
+ggsave(file = "Raw plots/Figure 6.1 (raw).pdf", plot = plot2, dpi = 300)
